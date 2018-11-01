14f255e64ee2236c1854aee38fadb7239fe0ee07
Fer Hernandez <fh.1111@gmail.com>
2018-11-01 00:04:15 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,10 +17,11 @@ end
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
-                  "porcelain vases" => 2, 
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
 
+  shipping_manifest["whale bone corsets"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
