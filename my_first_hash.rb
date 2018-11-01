bf2b9c93f2736c36873759718a47a9442579bf66
Fer Hernandez <fh.1111@gmail.com>
2018-11-01 00:07:22 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,6 +24,7 @@ def retrieval
   shipping_manifest["whale bone corsets"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -43,6 +44,6 @@ def adding
 
   # return the shipping_manifest hash below
 
-  shipping_manifest["oil paintings"]
+  
 
 end
